59b3e868fb60827d5de8e744a38936e2e6b792aa
Lokinorse <demonictype@gmail.com>
2020-02-03 06:27:03 +0300

    readme documentation added

--- index.php
+++ index.php
@@ -1,32 +1,24 @@
 <?php
-// You may need to amend this path to locate Composer's autoloader
 require('vendor/autoload.php'); 
 require_once('googleSpreadsheetsSettings.php');
 
 use seregazhuk\PinterestBot\Factories\PinterestBot;
 
-
 $bot = PinterestBot::create();
-// Login
 $bot->auth->login('demonictype@gmail.com', 'lok1norse');
-
 // Get lists of your boards
 $boards = $bot->boards->forUser('demonictype');
-// Create a pin
 
-/*     $bot->pins->create('https://www.dw.com/image/44504125_303.jpg', $boards[1]['id'], 'Pin description'); */
-function postPinsFromFile($url, $description){
+// Post pins from the file
+function postPinsFromFile($url, $description)
+{
     global $bot;
     global $boards;
-    sleep(61); //Задержка необходима во избежание бана
+    sleep(61); //delay is required to avoid ban
     $bot->pins->create($url, $boards[1]['id'], $description);
-
-
 }
 
-foreach ($values as $row) {
-
+foreach ($values as $row) 
+{
     postPinsFromFile($row[0], $row[1]);
-
-
 }
